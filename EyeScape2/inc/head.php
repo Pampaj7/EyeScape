6fdd4d7bc719bc09763c7fe9bf052598704602bb
Pampaj7 <leonardo.pampaloni1@stud.unifi.it>
2022-09-01 16:51:30 +0200

    initial commit

--- EyeScape2/inc/head.php
+++ EyeScape2/inc/head.php
@@ -11,7 +11,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Cabin+Condensed:700' rel='stylesheet' type='text/css'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
+    <script src="https://kit.fontawesome.com/b8ca21d9b2.js" crossorigin="anonymous"></script>
+    <script src="../scripts/jQuery-3.3.1.min.js"></script>
+    <script src="../scripts/jQuery.plugin.js"></script>
 </head>
 
 <body>
--- EyeScape2/index.php
+++ EyeScape2/index.php
@@ -1,5 +1,32 @@
 <?php include("./inc/head.php"); ?>
 
-<h1>TEST2</h1>
+<h1>LOGIN PAGE</h1>
+
+
+<div class="box1">
+    <div class="logo"><img src="../../../Documentation/Logo/Logo-completoTransparent.png"></div>
+    <form action="#" method="post">
+        <div class="field-group">
+            <span class="fa-user" aria-hidden="true"></span>
+            <div class="field">
+                <input name="username" id="user" type="text" value placeholder="Username" required>
+            </div>
+        </div>
+        <div class="field-group">
+            <span class="fa-lock" aria-hidden="true"></span>
+            <div class="field">
+                <input name="password" id="pwd" type="password" value placeholder="Password" required>
+
+            </div>
+        </div>
+        <div class="field">
+            <button type="submit"> Login</button>
+        </div>
+    </form>
+</div>
+
+<!-- Struttura -->
+
 
 <?php include("./inc/footer.php"); ?>
+
