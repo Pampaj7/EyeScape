9aa62e07b2a03ffcae8140fa6ba63410762ffc40
Pampaj7 <89346168+Pampaj7@users.noreply.github.com>
2022-09-01 15:42:07 +0200

    Add files via upload

---
+++ EyeScape2/inc/footer.php
@@ -0,0 +1,7 @@
+
+	<script src="https://kit.fontawesome.com/b8ca21d9b2.js" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="./assets/js/custom.js"></script>
+   </body>
+</html>
---
+++ EyeScape2/index.php
@@ -0,0 +1,5 @@
+<?php include("./inc/head.php"); ?>
+
+<h1>TEST</h1>
+
+<?php include("./inc/footer.php"); ?>
